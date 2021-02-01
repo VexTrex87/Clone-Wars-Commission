032b0f69af5fee41fbcc054c8d5107c6b19b7560
Vex87 <AndrewKyleGuilas@gmail.com>
2021-02-01 11:17:52 -0500

    Created morph module

---
+++ src/ServerScriptService/Morph.server.lua
@@ -0,0 +1,11 @@
+local Players = game:GetService("Players")
+local ServerStorage = game:GetService("ServerStorage")
+
+local stormTrooper = ServerStorage.Objects.Morphs.StormTrooper
+local morphPlayer = require(ServerStorage.Modules.MorphPlayer)
+
+Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        morphPlayer(character, stormTrooper)
+    end)
+end)
